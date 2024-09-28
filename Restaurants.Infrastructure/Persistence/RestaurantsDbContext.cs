7122e247e75bc6a25f465ba9bbee5e17fa2c1eb1
ZuraArabidze <z.arabidzee@gmail.com>
2024-09-28 15:02:56 +0400

    Installing Entity Framework and create migration

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+namespace Restaurants.Infrastructure.Persistence
+{
+    internal class RestaurantsDbContext: DbContext
+    {
+        internal DbSet<Restaurant>  Restaurants { get; set; }
+        internal DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RestaurantsDb;Trusted_Connection=True;",
+                    optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Restaurant>()
+                .OwnsOne(r => r.Address);
+
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(r => r.Dishies)
+                .WithOne()
+                .HasForeignKey(d => d.RestaurantId);
+              
+
+
+        }
+
+    }
+}

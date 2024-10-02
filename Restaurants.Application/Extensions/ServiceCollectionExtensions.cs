05992a1e55849de157afd43dc5be5446c7974bad
ZuraArabidze <z.arabidzee@gmail.com>
2024-10-02 14:05:55 +0400

    Fluent Validation

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
 namespace Restaurants.Application.Extensions
 {
@@ -6,9 +8,13 @@ namespace Restaurants.Application.Extensions
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            var applicationAssembly = typeof(ServiceCollectionServiceExtensions).Assembly;
             services.AddScoped<IRestaurantsService, RestaurantsService>();
 
-            services.AddAutoMapper(typeof(ServiceCollectionServiceExtensions).Assembly);
+            services.AddAutoMapper(applicationAssembly);
+
+            services.AddValidatorsFromAssemblies([applicationAssembly])
+                .AddFluentValidationAutoValidation();
         }
     }
 }
--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -9,19 +9,19 @@ namespace Restaurants.Application.Restaurants.Dtos
 {
     public class CreateRestaurantDto
     {
-        [StringLength(100,MinimumLength = 3)]
+        //[StringLength(100,MinimumLength = 3)]
         public string Name { get; set; } = default;
         public string Description { get; set; } = default;
-        [Required(ErrorMessage = "Insret a valid category")]
+       // [Required(ErrorMessage = "Insret a valid category")]
         public string Category { get; set; } = default;
         public bool hasDelivery { get; set; }
-        [EmailAddress(ErrorMessage = "Please provide a valid email address")]
+       // [EmailAddress(ErrorMessage = "Please provide a valid email address")]
         public string? ContactEmail { get; set; }
-        [Phone(ErrorMessage = "Please provide a valid phone number")]
+       // [Phone(ErrorMessage = "Please provide a valid phone number")]
         public string? ContactNumber { get; set; }
         public string? City { get; set; }
         public string? Street { get; set; }
-        [RegularExpression(@"^\d{2}-\d{3}$",ErrorMessage = "Please provide a valid postal code (XX-XXX).")]
+       // [RegularExpression(@"^\d{2}-\d{3}$",ErrorMessage = "Please provide a valid postal code (XX-XXX).")]
         public string? PostalCode { get; set; }
     }
 }
